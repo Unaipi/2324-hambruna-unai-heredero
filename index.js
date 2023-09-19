731b436fa2f02ef8342e3e4882337e35bb7df064
Unai <unai.heredero@ikasle.aeg.eus>
2023-09-19 09:07:11 +0200

    ariketa 1, 2, 3 terminadas, ariketa 4 por corregir

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const findDonutWithMostIron = (donuts) => {
 
     donuts.forEach((element) => {
         const ironValue = parseFloat(element.nutrition_facts.nutrition.vitamines.find(vitamin => vitamin.type === "Iron").percent);
-        
+
         if (ironValue > maxIronValue) {
             maxIronValue = ironValue;
             maxIronDonutName = element.name;
@@ -71,7 +71,7 @@ const findDonutWithMostProtein = (donuts) => {
 
 const findDonutsWithLeastFiber = (donuts) => {
     let minFiberDonuts = [];
-    let minFiberValue = 999999999999; 
+    let minFiberValue = 999999999999;
 
     donuts.forEach((element) => {
         const fiberValue = parseFloat(element.nutrition_facts.nutrition.carbohydrate.carbs_detail.type.fibre);
@@ -103,10 +103,14 @@ const listAllDonutsWithCalories = (donuts) => {
         const donutName = element.name;
         const calories = element.nutrition_facts.nutrition.calories;
 
-        donutsWithCalories.push({ name: donutName, calories: calories });
+        donutsWithCalories.push({
+            name: donutName,
+            calories: calories
+        });
     });
 
     return donutsWithCalories;
+
 };
 
 const listAllDonutsWithCarbohydrates = (donuts) => {
@@ -116,7 +120,10 @@ const listAllDonutsWithCarbohydrates = (donuts) => {
         const donutName = element.name;
         const carbohydrates = element.nutrition_facts.nutrition.carbohydrate.carbs_detail.amount;
 
-        donutsWithCarbohydrates.push({ name: donutName, carbohydrates: carbohydrates });
+        donutsWithCarbohydrates.push({
+            name: donutName,
+            carbohydrates: carbohydrates
+        });
     });
 
     return donutsWithCarbohydrates;
@@ -135,13 +142,13 @@ const calculateAverageCalories = (donuts) => {
     return averageCalories;
 };
 
-const calculateTotalSaturatedFat = (donuts) => {
+const calculateTotalSaturatedFat = (donuts) => { 
     let totalSaturatedFat = 0;
 
     donuts.forEach((element) => {
         const saturatedFat = parseFloat(element.nutrition_facts.nutrition.fat.fat_type.saturated);
-        
-        if (!isNaN(saturatedFat)) {
+
+        if (saturatedFat) {
             totalSaturatedFat += saturatedFat;
         }
     });
@@ -150,7 +157,7 @@ const calculateTotalSaturatedFat = (donuts) => {
 };
 
 const calculateAverageVitaminPercentages = (donuts) => {
-    const totalDonuts = donuts.length;
+    // const totalDonuts = donuts.length;
     const vitaminAArray = [];
     const vitaminCArray = [];
     const calciumArray = [];
@@ -158,7 +165,7 @@ const calculateAverageVitaminPercentages = (donuts) => {
 
     donuts.forEach((element) => {
         const vitamines = element.nutrition_facts.nutrition.vitamines;
-        
+
         vitamines.forEach((vitamin) => {
             if (vitamin.type === "Vitamin A") {
                 vitaminAArray.push(parseFloat(vitamin.percent));
@@ -177,7 +184,7 @@ const calculateAverageVitaminPercentages = (donuts) => {
             return 0;
         }
 
-        const totalPercentage = vitaminArray.reduce((acc, current) => acc + current);
+        const totalPercentage = vitaminArray.reduce((acc, cur) => acc + cur);
         return totalPercentage / vitaminArray.length;
     };
 
@@ -189,10 +196,70 @@ const calculateAverageVitaminPercentages = (donuts) => {
     };
 };
 
+//ariketa 3
+const listDonutBatters = (donuts) => {
+
+    const donutBatters = [];
+
+    donuts.forEach((element) => {
+        const donutName = element.name;
+        const batters = element.batters.batter.map(batter => batter.type);
+
+        donutBatters.push({
+            name: donutName,
+            batters: batters
+        });
+    });
+
+    return donutBatters;
+};
+
+
+const listDonutToppings = (donuts) => {
+
+    const donutToppings = [];
+
+    donuts.forEach((element) => {
+        const donutName = element.name;
+        const toppings = element.topping.map(element => element.type);
+
+        donutToppings.push({
+            name: donutName,
+            toppings: toppings
+        });
+    });
+
+    return donutToppings;
+};
+
+
+//ariketa 4(4 monedas de plata (ppu))
+
+const calculateDonutsAndChange = (donuts, budget) => {
+    const donutsCount = {};
+    let remainingBudget = budget;
+
+    donuts.forEach((element) => {
+        const donutName = element.name;
+        const donutPrice = element.ppu;
+
+        const donutsComprables = Math.floor(remainingBudget / donutPrice);
+        donutsCount[donutName] = donutsComprables;
+        remainingBudget -= donutsComprables * donutPrice;
+    });
+
+    return {
+        donutsCount: donutsCount,
+        remainingBudget: remainingBudget,
+    };
+};
+
 
 const fetchAndProcessDonuts = async () => {
     try {
         const result = await getAllDonuts();
+
+        const budget = 4;
         const donuts = result.items.item;
 
         const donutWithMostSugar = findDonutWithMostSugar(donuts);
@@ -201,46 +268,81 @@ const fetchAndProcessDonuts = async () => {
         const donutsConMenosFibra = findDonutsWithLeastFiber(donuts);
 
         const donutsConCalorias = listAllDonutsWithCalories(donuts);
-        const donutsConCarbohidratos = listAllDonutsWithCarbohydrates(donuts);  
+        const donutsConCarbohidratos = listAllDonutsWithCarbohydrates(donuts);
 
         const averageCalories = calculateAverageCalories(donuts); // Calcular la media de calorías
         const totalSaturatedFat = calculateTotalSaturatedFat(donuts);
 
         const averageVitaminPercentages = calculateAverageVitaminPercentages(donuts);
 
+        const donutBatters = listDonutBatters(donuts);
+
+        const donutToppings = listDonutToppings(donuts);
+
+        const donutsAndChange = calculateDonutsAndChange(donuts, budget)
         //ariketa 1
         console.log("Donut con más azúcar:", donutWithMostSugar.name);
         console.log("Cantidad de azúcar:", donutWithMostSugar.sugar);
-
+        console.log("")
         console.log("Donut con más hierro:", donutWithMostHierro.name);
         console.log("Cantidad de hierro:", donutWithMostHierro.iron);
-
+        console.log("")
         console.log("Donut con más proteína:", donutConMasProteina.name);
         console.log("Cantidad de proteína:", donutConMasProteina.protein);
-
+        console.log("")
         console.log("Donuts con menos fibra:", donutsConMenosFibra.donuts);
         console.log("Cantidad de fibra mínima:", donutsConMenosFibra.fiber);
-
+        console.log("")
 
         //ariketa 2
-        donutsConCalorias.forEach((donut) => {
-            console.log(`Donut: ${donut.name}, Calorías: ${donut.calories}`);
+        console.log("Donuts con sus calorías: ")
+        donutsConCalorias.forEach((element) => {
+            console.log(`Donut: ${element.name}, Calorías: ${element.calories}`);
         });
+        console.log("")
 
-        donutsConCarbohidratos.forEach((donut) => {
-            console.log(`Donut: ${donut.name}, Carbohidratos: ${donut.carbohydrates}`);
+        console.log("Donuts con sus Carbohidratos: ")
+        donutsConCarbohidratos.forEach((element) => {
+            console.log(`Donut: ${element.name}, Carbohidratos: ${element.carbohydrates}`);
+            console.log("")
         });
         console.log("Media de calorías de todos los donuts:", averageCalories);
         console.log("Suma de grasas saturadas de todos los donuts:", totalSaturatedFat + "g");
+        console.log("")
 
         console.log("Porcentaje promedio de Vitamina A:", averageVitaminPercentages.vitaminA + "%");
         console.log("Porcentaje promedio de Vitamina C:", averageVitaminPercentages.vitaminC + "%");
         console.log("Porcentaje promedio de Calcio:", averageVitaminPercentages.calcium + "%");
         console.log("Porcentaje promedio de Hierro:", averageVitaminPercentages.iron + "%");
+        console.log("")
+
+        console.log("Donuts con sus batters: ")
+        donutBatters.forEach((element) => {
+            console.log(`Donut: ${element.name}`);
+            console.log(`Batters: ${element.batters}`);
+            console.log("")
+        });
+
+        console.log("Donuts con sus toppings: ")
+        donutToppings.forEach((element) => {
+            console.log(`Donut: ${element.name}`);
+            console.log(`Toppings: ${element.toppings}`);
+            console.log("")
+        });
+
+        console.log("Cantidad de donuts que puedes comprar de cada tipo:");
+        for (const donutName in donutsAndChange.donutsCount) {
+            console.log(`${donutName}: ${donutsAndChange.donutsCount[donutName]}`);
+        }
+
+        console.log("Dinero restante:", donutsAndChange.remainingBudget + " unidades monetarias");
+
+
+
 
     } catch (error) {
         console.log(error.message);
-}
+    }
 }
 
 
